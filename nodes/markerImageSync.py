1d6480156107be387178f586d0e7dfd6f7cab42c
Agustín <guriandoro@gmail.com>
2020-07-05 22:09:55 -0300

    MarkerImageSync node now supports two more topics

--- nodes/markerImageSync.py
+++ nodes/markerImageSync.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 import message_filters
 import rospy
 
 from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 from ar_pose.msg import ARMarker
+from visualization_msgs.msg import Marker
 #cassandra_ros/MarkerImage
 from cassandra_ros.msg import MarkerImage
 
@@ -14,7 +16,7 @@ from cassandra_ros.msg import MarkerImage
 
 # This callback is used to merge partial marker and image data into
 # a new type 'cassandra_ros.MarkerImage'
-def callback(image, marker):
+def callback(image, marker, rviz):
   rospy.loginfo("--> Image and Marker read.")
   # MarkerImage type is:
   #Header header
@@ -27,9 +29,12 @@ def callback(image, marker):
   #uint32 image_step
   #uint8[] image_data
   
-  pub.publish(marker.header, marker.id, marker.pose.pose, 
-              image.height, image.width, image.encoding, 
-              image.is_bigendian, image.step, image.data)
+  pub_marker_image.publish(marker.header, marker.id, marker.pose.pose, 
+                           image.height, image.width, image.encoding, 
+                           image.is_bigendian, image.step, image.data)
+  
+  pub_marker.publish(rviz)
+  pub_image.publish(image)
 
 
 if __name__ == "__main__":
@@ -37,13 +42,17 @@ if __name__ == "__main__":
   
   image_sub = message_filters.Subscriber('/camera/image_rect', Image)
   marker_sub = message_filters.Subscriber('/ar_pose_marker', ARMarker)
+  rviz_sub = message_filters.Subscriber('/visualization_marker', Marker)
   
   rospy.loginfo("# Initializing Publisher...\n")
 
-  pub = rospy.Publisher('marker_out', MarkerImage, queue_size=100)
+  pub_marker_image = rospy.Publisher('sync_marker_image', MarkerImage, queue_size=100)
+  pub_marker = rospy.Publisher('sync_marker', Marker, queue_size=100)
+  pub_image = rospy.Publisher('sync_image', Image, queue_size=100)
+  
   rate = rospy.Rate(100) # 100hz
   
   rospy.loginfo("# Initializing TimeSynchronizer(image, marker)\n")
-  ts = message_filters.TimeSynchronizer([image_sub, marker_sub], queue_size=100)
+  ts = message_filters.TimeSynchronizer([image_sub, marker_sub, rviz_sub], queue_size=100)
   ts.registerCallback(callback)
   rospy.spin()
